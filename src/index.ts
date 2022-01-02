437b0d3e8dcab267e1e017aff477e83eb2a415be
JennyQ <jennifercquispe@gmail.com>
2022-01-01 19:28:02 -0500

    adding google maps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
 
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
 const user = new User();
 const company = new Company();
 
